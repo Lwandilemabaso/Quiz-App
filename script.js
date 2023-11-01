c8ffa63d4665f3528c3047eb3e0a7c73439245c8
Asiphile1 <142981380+Asiphile1@users.noreply.github.com>
2023-11-01 02:27:37 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,92 @@
 const question = [
   {
-    question: "",
+    questions: "What does HTML stand for?",
     answers: [
-      { text: "", correct: true },
-      { text: "", correct: false },
-      { text: "", correct: false },
-      { text: "", correct: false },
+      { text: "HyperText Markup Language", correct: true },
+      { text: "Hyperlink and Text Markup Language", correct: false },
+      { text: "High Text Multilanguage", correct: false },
+      { text: "Hyper Transfer Markup Language", correct: false },
     ],
   },
   {
-    question: "",
+    questions: "Which HTML element is used to define a hyperlink?",
     answers: [
-      { text: "", correct: true },
-      { text: "", correct: false },
-      { text: "", correct: false },
-      { text: "", correct: false },
+      { text: "<a>", correct: true },
+      { text: "<link>", correct: false },
+      { text: "<href>", correct: false },
+      { text: "<hlink>", correct: false },
+    ],
+  },
+  {
+    questions: "Which HTML tag is used to create an unordered list?",
+    answers: [
+      { text: "<ul>", correct: true },
+      { text: "<ol>", correct: false },
+      { text: "<li>", correct: false },
+      { text: "<list>", correct: false },
+    ],
+  },
+  {
+    questions: "What is the correct CSS syntax to select an element with the ID myElement?",
+    answers: [
+      { text: "#myElement", correct: true },
+      { text: ".myElement", correct: false },
+      { text: "element#myElement", correct: false },
+      { text: "element.myElement", correct: false },
+    ],
+  },
+  {
+    questions: "In CSS, how can you make text bold?",
+    answers: [
+      { text: "font-weight: bold;", correct: true },
+      { text: "text-bold: true;", correct: false },
+      { text: "bold-text: on;", correct: false },
+      { text: "text: bold;", correct: false },
+    ],
+  },
+  {
+    questions: "What CSS property is used to set the background color of an element?",
+    answers: [
+      { text: "background-color", correct: true },
+      { text: "color", correct: false },
+      { text: "bgcolor", correct: false },
+      { text: "bg-color", correct: false },
+    ],
+  },
+  {
+    questions: " Which keyword is used to declare a variable in JavaScript?",
+    answers: [
+      { text: "var", correct: true },
+      { text: "let", correct: false },
+      { text: "const", correct: false },
+      { text: "int", correct: false },
+    ],
+  },
+  {
+    questions: "What is the result of the expression 5 + 2 in JavaScript?",
+    answers: [
+      { text: "'52'", correct: true },
+      { text: "'7'", correct: false },
+      { text: "7", correct: false },
+      { text: "52", correct: false },
+    ],
+  },
+  {
+    questions: " What does the typeof operator in JavaScript return for an array?",
+    answers: [
+      { text: "object", correct: true },
+      { text: "array", correct: false },
+      { text: "list", correct: false },
+      { text: "collection", correct: false },
+    ],
+  },
+  {
+    questions: "What is the correct way to write a comment in JavaScript?",
+    answers: [
+      { text: "// This is a comment", correct: true },
+      { text: "/* This is a comment */", correct: false },
+      { text: "# This is a comment", correct: false },
+      { text: "-- This is a comment", correct: false },
     ],
   },
 ];
